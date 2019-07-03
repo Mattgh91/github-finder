fbdf3566e269a945786726e94a4941c6682a1459
Matthew Haynes <matthew.haynes@joipolloi.com>
2019-07-03 20:01:00 +0100

    Add clear button + alert functionality if empty search is attempted

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import { Navbar } from './components/layout/Navbar';
 import { Users } from './components/Users';
+import Alert from './components/Alert';
 import Search from './components/Search';
 import Spinner from './components/layout/Spinner';
 import './index.css';
@@ -9,21 +10,22 @@ import './index.css';
 class App extends Component {
     state = {
         users: [],
-        loading: true,
+        loading: false,
+        alert: null,
     };
 
-    async componentDidMount() {
-        const res = await axios.get(
-            `https://api.github.com/users?
-            client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&
-            client_secret=${process.env.REACT_APP_GITHUB_SECRET}`);
-
-        this.setState({
-            users: res.data,
-        }, () => {
-            this.setState({ loading: false });
-        });
-    }
+    // async componentDidMount() {
+    //     const res = await axios.get(
+    //         `https://api.github.com/users?
+    //         client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&
+    //         client_secret=${process.env.REACT_APP_GITHUB_SECRET}`);
+    //
+    //     this.setState({
+    //         users: res.data,
+    //     }, () => {
+    //         this.setState({ loading: false });
+    //     });
+    // }
 
     // Search GitHub Users...
     searchUsers = async text => {
@@ -41,13 +43,37 @@ class App extends Component {
         });
     };
 
+    clearUsers = () => {
+         this.setState({
+             users: [],
+             loading: false,
+         });
+    };
+
+    setAlert = (msg, type) => {
+        this.setState({
+            alert: {
+                msg,
+                type,
+            }
+        }, () => {
+            setTimeout(() => this.setState({ alert : null }), 3500)
+        })
+    };
+
     render() {
-        const { users, loading } = this.state;
+        const { users, loading, alert } = this.state;
         return (
             <div className="App">
                 <Navbar />
                 <div className="container">
-                    <Search searchUsers={this.searchUsers} />
+                    <Alert alert={alert} />
+                    <Search
+                        searchUsers={this.searchUsers}
+                        clearUsers={this.clearUsers}
+                        showClear={users.length > 0}
+                        setAlert={this.setAlert}
+                    />
                     <Spinner loading={loading} />
                     <Users users={users} loading={loading} />
                 </div>
--- src/components/Search.js
+++ src/components/Search.js
@@ -8,20 +8,29 @@ class Search extends Component {
 
     static propTypes = {
         searchUsers: PropTypes.func.isRequired,
+        clearUsers: PropTypes.func.isRequired,
+        showClear: PropTypes.bool.isRequired,
+        setAlert: PropTypes.func.isRequired,
     };
 
-    onChange = e => this.setState({ [e.target.name]: e.target.value });
+    onChange = e => this.setState({[e.target.name]: e.target.value});
 
     onSubmit = e => {
-        const { text } = this.state;
-        const { searchUsers } = this.props;
+        const {text} = this.state;
+        const {searchUsers, setAlert} = this.props;
 
         e.preventDefault();
-        searchUsers(text);
-        this.setState({ text: '' });
+        if(text === '') {
+            setAlert('Please enter something', 'light');
+        } else {
+            searchUsers(text);
+            this.setState({text: ''});
+        }
     };
 
     render() {
+        const {clearUsers, showClear} = this.props;
+        const {text} = this.state;
         return (
             <div>
                 <form className="form" onSubmit={this.onSubmit}>
@@ -29,7 +38,7 @@ class Search extends Component {
                         type="text"
                         name="text"
                         placeholder="Search Users..."
-                        value={this.state.text}
+                        value={text}
                         onChange={this.onChange}
                     />
                     <input
@@ -38,6 +47,14 @@ class Search extends Component {
                         className="btn btn-dark btn-block"
                     />
                 </form>
+                {showClear &&
+                    <button
+                        className="btn btn-light btn-block"
+                        onClick={clearUsers}
+                    >
+                        Clear
+                    </button>
+                }
             </div>
         );
     }
