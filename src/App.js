1f328e3c815e583b854feac02c17a510c97e1007
Mattgh91 <mattgh9152@gmail.com>
2019-07-10 21:58:45 +0100

    Add netlify deployment + prod global variables.. add NotFound (404) page... Neaten up app.js a little

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React, {Fragment} from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Navbar } from './components/layout/Navbar';
-import { Users } from './components/Users';
+import Home from './components/Home';
 import User from './components/User';
 import Alert from './components/Alert';
-import Search from './components/Search';
 import About from './components/About';
+import NotFound from './components/NotFound';
 import './index.css';
 
 import GithubState from './context/github/GithubState';
@@ -21,12 +21,7 @@ const App = () => {
                     <div className="container">
                         <Alert alert={alert} />
                         <Switch>
-                            <Route exact path='/' render={props => (
-                                <Fragment>
-                                    <Search />
-                                    <Users />
-                                </Fragment>
-                            )} />
+                            <Route exact path='/' component={Home} />
                             <Route exact path='/about' component={About} />
                             <Route
                                 exact
@@ -37,6 +32,7 @@ const App = () => {
                                     />
                                 )}
                             />
+                            <Route component={NotFound} />
                         </Switch>
                     </div>
                 </div>
